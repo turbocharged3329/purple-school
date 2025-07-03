f1d6e5c21290c83114cbe7323ce785575439c063
kitaev_v <kitaev@reezon.ly>
2025-07-03 18:15:03 +0300

    Добавлен хедер и компонент для отображение количества очков

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <main class="g-main-container">
+    <GHeader title="Запомни слово">
+      <template #right>
+        <GScore :model-value="100" />
+      </template>
+    </GHeader>
+
     <GButton>Начать игру</GButton>
   </main>
 </template>
 
 <script setup>
 import GButton from './components/GButton.vue';
+import GHeader from './components/GHeader.vue';
+import GScore from './components/GScore.vue';
 </script>
 
 
---
+++ src/components/GHeader.vue
@@ -0,0 +1,42 @@
+<template>
+    <header class="g-header">
+        <section class="g-header-left">
+            <h1 class="g-header__title">
+                {{ title }}
+            </h1>
+        </section>
+
+        <section class="g-header-right">
+            <slot name="right"></slot>
+        </section>
+    </header>
+</template>
+
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    }
+})
+</script>
+
+<style scoped>
+.g-header {
+    width: 100%;
+    height: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 3rem 4.125rem;
+    box-sizing: border-box;
+}
+
+.g-header__title {
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: 12%;
+}
+</style>
